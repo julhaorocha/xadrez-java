785aaff07757913e08514a4a5999be949e6e1a43
Julio Rocha <julhaorocha@gmail.com>
2019-05-27 16:45:35 -0300

    Impressão de peças coloridas

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -27,9 +27,19 @@ public class PartidaXadrez {
 		tabuleiro.lugarPeca(peca, new PosicaoXadrez(coluna, linha).cvPosicao());
 	}
 	private void configIni() {
-		lugarNovaPeca('b', 6, new Torre(tabuleiro, Cor.BRANCO));
-		lugarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
-		lugarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+		lugarNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+		lugarNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+		lugarNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+		lugarNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+		lugarNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+		lugarNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+
+		lugarNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
+		lugarNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
+		lugarNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
+		lugarNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+		lugarNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
+		lugarNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
 	}
 
 }
